50ec652dae9ba559da9761767b4d1f02901fe41b
mhtrmkholis <mhtrmkholis@gmail.com>
2022-11-05 12:36:58 +0700

    test

--- shooper-react-native/App.js
+++ shooper-react-native/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import Navigation from './src/navigation';
 import reducer from './src/stores/reducers';
+// import 
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
